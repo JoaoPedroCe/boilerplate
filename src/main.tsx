6cdfceacc10f92c24c86b6fa0f8aeb9f6b59135b
Your Name <you@example.com>
2025-02-12 23:23:38 -0300

    feat - adicionar tanstack router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { routeTree } from './routeTree.gen.ts'
+import { createRouter, RouterProvider } from '@tanstack/react-router'
+import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+
+const router = createRouter({ routeTree })
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <TanStackRouterDevtools router={router}/> 
+    <RouterProvider router={router} />
   </StrictMode>,
 )
